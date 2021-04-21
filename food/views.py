06114973666bd71402c1f47f2ffce6a758903522
Rokas <rookas.rudzenskas@gmail.com>
2021-04-21 15:32:29 +0300

    Rokas

--- food/views.py
+++ food/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import item
+from  django.template import loader
 
 
-# Create your views here.
+# Create your viewss here.
 
 def home(request):
-    return HttpResponse("Rokas!")
+    # item_list = item.Objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def item(request):
