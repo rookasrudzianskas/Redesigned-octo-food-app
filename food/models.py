5f319bb2f345d23b0f186acb5593d63cb28b488c
Rokas <rookas.rudzenskas@gmail.com>
2021-04-21 14:54:04 +0300

    Rokas

--- food/models.py
+++ food/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_desc = models.CharField(max_length=200)
+    item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name + ' ' + self.item_desc
